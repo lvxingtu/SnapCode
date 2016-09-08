62e2a18f87a9467cc0183c67ef675c571e182655
Jeff Martin <jeff@reportmill.com>
2016-09-08 10:04:52 -0500

    JavaPage: Rolled JPJavaTextPane in here.

--- src/snap/javatext/JavaPage.java
+++ src/snap/javatext/JavaPage.java
@@ -1,4 +1,7 @@
 package snap.javatext;
+import snap.app.AppBrowser;
+import snap.app.AppPane;
+import snap.app.SupportTray;
 import snap.gfx.TextBoxLine;
 import snap.javaparse.*;
 import snap.project.*;
@@ -10,15 +13,20 @@ import snap.web.*;
 /**
  * A JavaPage subclass to view/edit Java files.
  */
-public class JavaPage extends WebPage {
+public class JavaPage extends WebPage implements WebFile.Updater {
 
     // The JavaTextPane
-    JavaTextPane           _jtextPane = new JavaTextPane();
+    JavaTextPane           _jtextPane = new JPJavaTextPane();
 
 /**
  * Creates a new JavaPage.
  */
 public JavaPage()  { _jtextPane._javaPage = this; }
+
+/**
+ * Return the AppPane.
+ */
+AppPane getAppPane()  { return getBrowser() instanceof AppBrowser? ((AppBrowser)getBrowser()).getAppPane() : null; }
 
 /**
  * Returns the JavaTextView.
@@ -120,6 +128,125 @@ protected WebFile createNewFile(String aPath)
     sb.append("public class ").append(file.getSimpleName()).append(" extends Object {\n\n\n\n}");
     file.setText(sb.toString());
     return file;
+}
+
+/**
+ * Override to set selection using browser.
+ */
+private void setTextSel(int aStart, int anEnd)
+{
+    String urls = getFile().getURL().getString() + String.format("#Sel=%d-%d", aStart, anEnd);
+    getBrowser().setURLString(urls);
+}
+
+/**
+ * Override to open declaration.
+ */
+private void openDeclaration(JNode aNode)
+{
+    JavaDecl decl = aNode.getDecl();
+    if(decl!=null) openDecl(decl);
+}
+
+/**
+ * Open a super declaration.
+ */
+private void openSuperDeclaration(JMemberDecl aMemberDecl)
+{
+    JavaDecl sdecl = aMemberDecl.getSuperDecl(); if(sdecl==null) return;
+    openDecl(sdecl);
+}
+
+/**
+ * Opens a project declaration.
+ */
+private void openDecl(JavaDecl aDecl)
+{
+    // Get class name (if Java class, open Java source)
+    String cname = aDecl.getClassName(); if(cname==null) return;
+    if(cname.startsWith("java.") || cname.startsWith("javax.") || cname.startsWith("javafx.")) {
+        openJavaDecl(aDecl); return; }
+
+    // Open decl in project file
+    Project proj = Project.get(getSite()); if(proj==null) return;
+    WebFile file = proj.getProjectSet().getJavaFile(cname); if(file==null) return;
+    JavaData jdata = JavaData.get(file);
+    JNode node = JavaDecl.getDeclMatch(proj, jdata.getJFile(), aDecl);
+    String urls = file.getURL().getString();
+    if(node!=null) urls += String.format("#Sel=%d-%d", node.getStart(), node.getEnd());
+    getBrowser().setURLString(urls);
+}
+
+/**
+ * Override to open declaration.
+ */
+private void openJavaDecl(JavaDecl aDecl)
+{
+    String cname = aDecl.getClassName(); if(cname==null) return;
+    String cpath = '/' + cname.replace('.', '/') + ".java";
+    WebURL jurl = WebURL.getURL("http://reportmill.com/jars/8u05/src.zip!" + cpath);
+    if(cname.startsWith("javafx.")) jurl = WebURL.getURL("http://reportmill.com/jars/8u05/javafx-src.zip!" + cpath);
+    String urls = jurl.getString() + "#Member=" + aDecl.getSimpleName();
+    getBrowser().setURLString(urls);
+}
+
+/**
+ * Show references for given node.
+ */
+private void showReferences(JNode aNode)
+{
+    if(getAppPane()==null) return;
+    getAppPane().getSearchPane().searchReference(aNode);
+    getAppPane().setSupportTrayIndex(SupportTray.SEARCH_PANE);
+}
+
+/**
+ * Show declarations for given node.
+ */
+private void showDeclarations(JNode aNode)
+{
+    if(getAppPane()==null) return;
+    getAppPane().getSearchPane().searchDeclaration(aNode);
+    getAppPane().setSupportTrayIndex(SupportTray.SEARCH_PANE);
+}
+
+/** Override to update Page.Modified. */
+private void setTextModified(boolean aFlag)  { getFile().setUpdater(aFlag? this : null); }
+
+/** WebFile.Updater method. */
+public void updateFile(WebFile aFile)  { getFile().setText(getTextView().getText()); }
+
+/** Override to get ProgramCounter from ProcPane. */
+private int getProgramCounterLine()
+{
+    AppPane ap = getAppPane(); return ap!=null? ap.getProcPane().getProgramCounter(getFile()) : -1;
+}
+
+/**
+ * A JavaTextPane for a JavaPage to implement symbol features and such.
+ */
+public class JPJavaTextPane extends JavaTextPane {
+
+    /** Override to set selection using browser. */
+    public void setTextSel(int aStart, int anEnd)  { JavaPage.this.setTextSel(aStart, anEnd); }
+    
+    /** Override to open declaration. */
+    public void openDeclaration(JNode aNode)  { JavaPage.this.openDeclaration(aNode); }
+    
+    /** Open a super declaration. */
+    public void openSuperDeclaration(JMemberDecl aMemberDecl)  { JavaPage.this.openSuperDeclaration(aMemberDecl); }
+    
+    /** Show references for given node. */
+    public void showReferences(JNode aNode)  { JavaPage.this.showReferences(aNode); }
+    
+    /** Show declarations for given node. */
+    public void showDeclarations(JNode aNode)  { JavaPage.this.showDeclarations(aNode); }
+    
+    /** Override to update Page.Modified. */
+    public void setTextModified(boolean aFlag)  { super.setTextModified(aFlag); JavaPage.this.setTextModified(aFlag); }
+    
+    /** Override to get ProgramCounter from ProcPane. */
+    public int getProgramCounterLine()  { return JavaPage.this.getProgramCounterLine(); }
 }
 
 }
