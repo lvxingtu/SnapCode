b6484ec0233ab8cc66c626aeb48657d2e580a3ae
Jeff Martin <jeff@reportmill.com>
2017-05-08 14:13:55 -0500

    Change back.

--- src/snap/javaparse/JMethodDecl.java
+++ src/snap/javaparse/JMethodDecl.java
@@ -90,7 +90,7 @@ public JavaDecl[] getParamTypes()
 {
     JavaDecl ptypes[] = new JavaDecl[_params.size()];
     for(int i=0, iMax=_params.size(); i<iMax; i++) { JVarDecl vd = _params.get(i);
-        ptypes[i] = vd.getDecl(); }
+        ptypes[i] = vd.getEvalType(); }
     return ptypes;
 }
 
